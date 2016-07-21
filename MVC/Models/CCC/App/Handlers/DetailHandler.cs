c34f845184a64653907d1a42d313375b3d165dff
Eric Rovtar <eric.rovtar@me.com>
2016-07-21 16:02:18 -0400

    Create Detail Handler and Media Detail Handler models

---
+++ MVC/Models/CCC/App/Handlers/DetailHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using CCC.Models.App.Objects;
+using CCC.Models.App.Items;
+
+namespace CCC.Models.App.Handlers
+{
+    public partial class DetailHandler : Handler
+    {
+        /// <summary>
+        /// Simple container object holding a single array of ITEMs. Items defined here are used to populate the sharing menu, allowing the user to share content on various services (Email, SMS, Facebook, Twitter, etc).
+        /// In most cases, the actionSheet defines exactly two items, with handlers = defaultShare and htmlShare.
+        /// </summary>
+        public IEnumerable<Item> ActionSheet { get; set; }
+
+        /// <summary>
+        /// Defines the main text content used for this Detail screen, such as a long-form description.
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// An array of IMAGE objects defining image resources for the Detail screen. If more than one image object is provided, the device will choose the image size which is most appropriate for the current visual context on that specific device.
+        /// </summary>
+        public IEnumerable<Image> Images { get; set; }
+
+        /// <summary>
+        /// Subtitle text content for this Detail screen.
+        /// </summary>
+        public string Subtitle { get; set; }
+
+        /// <summary>
+        /// Title text content for this Detail screen.
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
---
+++ MVC/Models/CCC/App/Handlers/MediaDetailHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using CCC.Models.App.Objects;
+using CCC.Models.App.Items;
+
+namespace CCC.Models.App.Handlers
+{
+    public partial class MediaDetailHandler : DetailHandler
+    {
+        /// <summary>
+        /// A date, rendered according to the device's native locale settings, and displayed along with extraSubtitles content. If a date is provided, it may reduce the number of extraSubtitles strings which can be displayed on the screen.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// An array of strings defining extra subtitle content for this object. This is your secret weapon for those times when one subtitle just isn't enough.
+        /// </summary>
+        public IEnumerable<string> ExtraSubtitles { get; set; }
+
+        /// <summary>
+        /// Multiple ITEMGROUPs may be combined to create groupings of buttons or other items. For the Media Detail page in the app, the ITEMGROUP at position = 0 describes buttons which appear above the description text, while the ITEMGROUP at position = 1 describes buttons which appear below the description text.
+        /// </summary>
+        public IEnumerable<Item> ItemGroups { get; set; }
+
+        /// <summary>
+        /// An array of audio and/or video MEDIA items. The app will select the appropriate url from the MEDIA objects defined here to match the requested user action and device context.        /// </summary>
+        public IEnumerable<Media> Media { get; set; }
+    }
+}
