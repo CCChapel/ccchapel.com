3e3ff5e52d22961052bfa426f0693c04c18d5fe3
Eric Rovtar <eric.rovtar@me.com>
2016-07-28 11:29:49 -0400

    Add Sharing Actions

---
+++ MVC/Models/CCC/App/Actions/ShareAction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CCC.Models.App.Actions
+{
+    /// <summary>
+    /// When handler = defaultShare, the body property should contain only plain-text content.This share object will be used for simple sharing services such as SMS (text messaging), or when htmlShare content is unavailable.
+    ///
+    /// When handler = htmlShare, the body property can contain rich HTML-formatted text.This share object will be used for services which support rich sharing such as Email.
+    /// </summary>
+    public class ShareAction : Action
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        new public enum Handlers
+        {
+            [EnumMember(Value = "defaultShare")]
+            DefaultShare,
+
+            [EnumMember(Value = "htmlShare")]
+            HtmlShare
+        }
+
+        /// <summary>
+        /// The name of the Handler invoked by this action. Any handler name is syntactically valid, though all handlers may not be supported in all contexts. Common values include list and mediaDetail.
+        /// </summary>
+        new public Handlers Handler { get; set; }
+
+        /// <summary>
+        /// The body contains the share text that you wish to pass off to an external share service. Depending on the particular service chosen by the user, the url value may be passed separately or appended to the body text so that you can share a link along with your text content.
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
---
+++ MVC/Models/CCC/App/Objects/ActionSheet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CCC.Models.App.Objects
+{
+    public partial class ActionSheet
+    {
+        protected List<ActionSheetActions> _items = new List<ActionSheetActions>();
+        public List<ActionSheetActions> Items
+        {
+            get
+            {
+                return _items;
+            }
+            set
+            {
+                _items = value;
+            }
+        }
+    }
+
+    public partial class ActionSheetActions
+    {
+        protected List<Actions.ShareAction> _actions = new List<Actions.ShareAction>();
+        public List<Actions.ShareAction> Actions
+        {
+            get
+            {
+                return _actions;
+            }
+            set
+            {
+                _actions = value;
+            }
+        }
+    }
+}
