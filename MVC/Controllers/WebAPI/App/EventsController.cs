5288dc60bafb18685a24dd0d30a7b559df89c367
Eric Rovtar <eric.rovtar@me.com>
2016-08-08 10:45:35 -0400

    Test Events Controller and Cookies

--- MVC/Controllers/WebAPI/App/EventsController.cs
+++ MVC/Controllers/WebAPI/App/EventsController.cs
@@ -55,8 +55,24 @@ namespace MVC.Controllers.WebAPI.App
                 Title = "Banner"
             });
 
+            //CookieHelpers.SetGlobalCookie(1);
+
             //Get Items
-            
+            List<CCC.Models.App.Actions.ListAction> actions = new List<CCC.Models.App.Actions.ListAction>();
+            actions.Add(new CCC.Models.App.Actions.ListAction()
+            {
+                Handler = CCC.Models.App.Actions.Action.Handlers.List,
+                Style = CCC.Models.App.Actions.ListAction.Styles.Plain,
+                Type = CCC.Models.App.Actions.Action.ActionTypes.Navigation,
+                Url = "http://beta.ccchapel.com/feeds/app/events/list"
+            });
+            List<ListItem> items = new List<ListItem>();
+            items.Add(new ListItem()
+            {
+                Title = CookieHelpers.DefaultCampusID.ToString(), //"Carousel Test",
+                Subtitle = CookieHelpers.LastVisit.ToShortDateString(), //"Testing",
+                Actions = actions
+            });
 
             //Create Handler
             ListHandler handler = new ListHandler()
@@ -67,7 +83,7 @@ namespace MVC.Controllers.WebAPI.App
                     Style = Header.HeaderStyles.Banner,
                     Items = bannerItems
                 },
-                //Items = items,
+                Items = items,
                 Title = "Events",
                 TrackViewedItems = false,
                 GridLayout = ListHandler.GridLayouts.Square,
@@ -75,6 +91,125 @@ namespace MVC.Controllers.WebAPI.App
             };
 
             return handler;
+        }
+
+        [AcceptVerbs("GET")]
+        public ListHandler List()
+        {
+            //Create imag array
+            List<Image> images = new List<Image>();
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1024x1024.jpg",
+                Width = 1024
+            });
+
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1536x560.jpg",
+                Width = 1536
+            });
+
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1920x692.jpg",
+                Width = 1920
+            });
+
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1920x1080.jpg",
+                Width = 1920
+            });
+
+            //Create Banner Items
+            List<Item> bannerItems = new List<Item>();
+            bannerItems.Add(new Item()
+            {
+                Images = images,
+                Title = "Banner"
+            });
+
+            //Get Items
+            List<ListItem> items = new List<ListItem>();
+            var events = CCB.PublicCalendar.GetEvents();
+
+            foreach (var ev in events.Items)
+            {
+                items.Add(ev.ListItem());
+            }
+
+            //Create Handler
+            ListHandler handler = new ListHandler()
+            {
+                Header = new Header()
+                {
+                    Title = "Events",
+                    Style = Header.HeaderStyles.Banner,
+                    Items = bannerItems
+                },
+                Items = items,
+                Title = "Events",
+                TrackViewedItems = false,
+            };
+
+            return handler;
+        }
+
+        [AcceptVerbs("GET")]
+        public Content.CarouselContent Carousel()
+        {
+            //Create imag array
+            List<Image> images = new List<Image>();
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1024x1024.jpg",
+                Width = 1024
+            });
+
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1536x560.jpg",
+                Width = 1536
+            });
+
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1920x692.jpg",
+                Width = 1920
+            });
+
+            images.Add(new Image()
+            {
+                Url = "http://beta.ccchapel.com/Content/2016-Q1/images/app/test/1920x1080.jpg",
+                Width = 1920
+            });
+
+            //Create items array
+            List<CCC.Models.App.Actions.Action> actions = new List<CCC.Models.App.Actions.Action>();
+            actions.Add(new CCC.Models.App.Actions.BrowserAction()
+            {
+                Handler = CCC.Models.App.Actions.BrowserAction.Handlers.Browser,
+                Style = CCC.Models.App.Actions.BrowserAction.Styles.Internal,
+                ContentUrl = "http://google.com"
+                
+            });
+            List<Item> items = new List<Item>();
+            items.Add(new Item()
+            {
+                Title = "Item",
+                Actions = actions
+            });
+
+            var carousel = new Content.CarouselContent()
+            {
+                Title = "Title",
+                Body = "Body",
+                Images = images,
+                Items = items
+            };
+
+            return carousel;
         }
     }
 }
